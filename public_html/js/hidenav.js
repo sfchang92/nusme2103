9221fc99a4b194a7c31844d91dbb43a42c116287
sfchang92 <takumifujiwara_bee1992@techgeek.co.in>
2015-09-11 21:36:02 +0800

    Initialization of website on GIT
    First commit

---
+++ public_html/js/hidenav.js
@@ -0,0 +1,39 @@
+	  // Hide Header on on scroll down
+	  var didScroll;
+	  var lastScrollTop = 0;
+	  var delta = 48;
+	  var navbarHeight = $('header').outerHeight();
+	  var st;
+	  $(window).scroll(function(){
+		  "use strict";
+		  didScroll = true;
+	  });
+  		  		  		  	  
+	  setInterval(function() {
+		  "use strict";
+		  if (didScroll) {
+			  hasScrolled();
+			  didScroll = false;
+		  }
+	  }, 250);
+	  function hasScrolled() {
+		  "use strict";
+		  st = $(document).scrollTop();			  
+		  // Make sure they scroll more than delta
+		  if(Math.abs(lastScrollTop - st) <= delta)
+	      {return;}
+		  // If they scrolled down and are past the navbar, add class .nav-up.
+		  // This is necessary so you never see what is "behind" the navbar.
+		  if (st > lastScrollTop && st > navbarHeight){
+			  // Scroll Down
+			  $('header').addClass('nav-up');
+			  $('footer').addClass('foot-down');		  
+		  } else {
+			  // Scroll Up
+			  if(st + $(window).height() < $(document).height()) {
+				  $('header').removeClass('nav-up');
+				  $('footer').removeClass('foot-down');	
+			  }
+		  }
+		  lastScrollTop = st;
+	  }
